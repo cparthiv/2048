551ba0ce55acb266f77479b230cb01202ce8fcea
Parthiv Mothipati <pmothipati@gmail.com>
2024-07-13 11:16:06 -0700

    run 100 games with varying depths

--- play.py
+++ play.py
@@ -1,8 +1,11 @@
-import sys, pygame
+import sys
+import pygame
 import model
 from math import log2
 from ai import *
+from time import sleep
 import multiprocessing as mp
+import os
 ai = False
 depth = 2
 
@@ -12,14 +15,16 @@ playRegion = 480, 480
 FPS = 60
 
 # Colors
-black = (0,0,0)
-white = (255,255,255)
+black = (0, 0, 0)
+white = (255, 255, 255)
 fontColor = (82, 52, 42)
-defaultTileColor = (232,232,232)
+defaultTileColor = (232, 232, 232)
 tileBoarderColor = fontColor
 
 # Game
 boardSize = 4
+
+
 def drawBoard(screen, board):
     screen.fill(black)
     for i in range(board.boardSize):
@@ -27,13 +32,14 @@ def drawBoard(screen, board):
             color = defaultTileColor
             numberText = ''
             if board.board[i][j] != 0:
-                gComponent = 235 - log2(board.board[i][j])*((235 - 52)/(board.boardSize**2))
-                color = (235, gComponent,52)
+                gComponent = 235 - \
+                    log2(board.board[i][j])*((235 - 52)/(board.boardSize**2))
+                color = (235, gComponent, 52)
                 numberText = str(board.board[i][j])
             rect = pygame.Rect(j*playRegion[0]/board.boardSize,
-                                i*playRegion[1]/board.boardSize,
-                                playRegion[0]/board.boardSize,
-                                playRegion[1]/board.boardSize)
+                               i*playRegion[1]/board.boardSize,
+                               playRegion[0]/board.boardSize,
+                               playRegion[1]/board.boardSize)
 
             pygame.draw.rect(screen, color, rect)
             pygame.draw.rect(screen, fontColor, rect, 1)
@@ -41,21 +47,23 @@ def drawBoard(screen, board):
             fontImage = tileFont.render(numberText, 0, fontColor)
             if fontImage.get_width() > playRegion[0]/board.boardSize:
                 fontImage = pygame.transform.scale(fontImage,
-                            (playRegion[0]/board.boardSize,
-                            fontImage.get_height()/fontImage.get_width()*playRegion[0]/board.boardSize))
+                                                   (playRegion[0]/board.boardSize,
+                                                    fontImage.get_height()/fontImage.get_width()*playRegion[0]/board.boardSize))
             screen.blit(fontImage,
-                    (j*playRegion[0]/board.boardSize + (playRegion[0]/board.boardSize - fontImage.get_width())/2,
-                    i*playRegion[1]/board.boardSize + (playRegion[1]/board.boardSize - fontImage.get_height())/2))
-    fontImage = scoreFont.render("Score: {:,}".format(board.score) + (" [AI enabled, depth={}]".format(depth) if ai else ""), 1, white)
+                        (j*playRegion[0]/board.boardSize + (playRegion[0]/board.boardSize - fontImage.get_width())/2,
+                         i*playRegion[1]/board.boardSize + (playRegion[1]/board.boardSize - fontImage.get_height())/2))
+    fontImage = scoreFont.render("Score: {:,}".format(
+        board.score) + (" [AI enabled, depth={}]".format(depth) if ai else ""), 1, white)
     screen.blit(fontImage, (1, playRegion[1]+1))
+
 
 def handleInput(event, board):
     global ai
 
     if event.type == pygame.QUIT:
-            pool.close()
-            pool.terminate()
-            sys.exit()
+        pool.close()
+        pool.terminate()
+        sys.exit()
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_RIGHT:
             board.move(model.RIGHT)
@@ -72,26 +80,38 @@ def handleInput(event, board):
             pool.terminate()
             sys.exit()
         elif event.key == pygame.K_SPACE:
-                ai = not ai
+            ai = not ai
 
     return board
+
+
+games = []
+
 
 def gameLoop():
     global depth
     clock = pygame.time.Clock()
-    board = model.Board(boardSize)
+    moves = 0
+    for depth in range(1, 10):
+        for game in range(10):
+            board = model.Board(boardSize)
+            while True:
 
-    while 1:
-        for event in pygame.event.get():
-            board = handleInput(event, board)
+                if not board.checkLoss():
+                    nextBestMove = getNextBestMoveExpectiminimax(
+                        board, pool, depth)
+                    board.move(nextBestMove)
+                    moves += 1
+                else:
+                    print(board.score)
+                    games.append([depth, board.score])
+                    os.system('cls')
+                    print(games)
+                    sleep(1)
+                    break
+                if moves % 100 == 0:
+                    print('Move', moves, 'Score', board.score)
 
-        if ai and not board.checkLoss():
-            nextBestMove = getNextBestMoveExpectiminimax(board, pool, depth)
-            board.move(nextBestMove)
-
-        drawBoard(screen, board)
-        pygame.display.flip()
-        clock.tick(FPS)
 
 if __name__ == '__main__':
     global screen
@@ -100,11 +120,11 @@ if __name__ == '__main__':
     global pool
     mp.freeze_support()
     mp.set_start_method('spawn')
-    pool = mp.Pool(processes=4)
+    pool = mp.Pool(processes=8)
 
-    pygame.init()
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("2048")
-    tileFont = pygame.font.SysFont("", 72)
-    scoreFont = pygame.font.SysFont("", 22)
+    # pygame.init()
+    # screen = pygame.display.set_mode(size)
+    # pygame.display.set_caption("2048")
+    # tileFont = pygame.font.SysFont("", 72)
+    # scoreFont = pygame.font.SysFont("", 22)
     gameLoop()
